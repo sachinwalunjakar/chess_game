b91366a4f0e5b07dfc724f754dbad6240381d52d
Sachin Santaram Walunjakar <walunjakarsachin@gmail.com>
2022-01-07 08:09:42 +0530

    code without error

---
+++ lib/backend files/model.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+enum PieceType { none, king, queen, camel, horse, elephant, soldier }
+
+enum Player { Player1, Player2, NullPlayer }
+
+class ChessPiece {
+  PieceType pieceType;
+  Player player;
+  ChessPiece(
+    this.pieceType,
+    this.player,
+  );
+  ChessPiece.copy(ChessPiece chessPiece)
+      : this(chessPiece.pieceType, chessPiece.player);
+
+  String get imagePath {
+    if (this.player == Player.Player1) {
+      switch (this.pieceType) {
+        case PieceType.none:
+          return "";
+        case PieceType.camel:
+          return "assets/black_camel.svg";
+        case PieceType.elephant:
+          return "assets/black_elephant.svg";
+        case PieceType.horse:
+          return "assets/black_horse.svg";
+        case PieceType.king:
+          return "assets/black_king.svg";
+        case PieceType.soldier:
+          return "assets/black_pawn.svg";
+        case PieceType.queen:
+          return "assets/black_queen.svg";
+        default:
+      }
+    } else if (this.player == Player.Player2) {
+      switch (this.pieceType) {
+        case PieceType.none:
+          return "";
+        case PieceType.camel:
+          return "assets/white_camel.svg";
+        case PieceType.elephant:
+          return "assets/white_elephant.svg";
+        case PieceType.horse:
+          return "assets/white_horse.svg";
+        case PieceType.king:
+          return "assets/white_king.svg";
+        case PieceType.soldier:
+          return "assets/white_pawn.svg";
+        case PieceType.queen:
+          return "assets/white_queen.svg";
+        default:
+      }
+    }
+    return "";
+  }
+  //
+}
+
+class StateColor {
+  static const Color normal = Colors.green;
+  static const Color possible = Colors.yellow;
+  static const Color selected = Colors.blueAccent;
+  static const Color susKill = Colors.redAccent;
+}
+
+//actual place in chess grid
+class Place {
+  final int position;
+  ChessPiece chessPiece;
+  Color stateColor;
+  Place({
+    required this.position,
+    required this.chessPiece,
+    required this.stateColor,
+  });
+
+  //return value instead reference
+  Place.copy(Place place)
+      : this(
+          position: place.position,
+          chessPiece: ChessPiece.copy(place.chessPiece),
+          stateColor: place.stateColor,
+        );
+}
+
+// class ChessGame {
+//   List<Place> chess = [];
+//   int selectedPos = -1;
+//   Player currentlyPlaying = Player.Player1;
+//   List<ChessPiece> killPieceOfPlayer1 = [];
+//   List<ChessPiece> killPieceOfPlayer2 = [];
+// }
+
+
+// @override
+//   String toString() {
+//     String state;
+//     switch (this.chessPiece.pieceType) {
+//       case PieceType.elephant:
+//         state = "elephant";
+//         break;
+//       case PieceType.horse:
+//         state = "horse";
+//         break;
+//       case PieceType.camel:
+//         state = "camel";
+//         break;
+//       case PieceType.king:
+//         state = "king";
+//         break;
+//       case PieceType.queen:
+//         state = "queen";
+//         break;
+//       case PieceType.soldier:
+//         state = "soldier";
+//         break;
+//       default:
+//         state = "empty";
+//     }
+//     if (this.position % 8 == 0) {
+//       return "\n" + state;
+//     }
+//     return state;
+//   }
---
+++ lib/chess_widget.dart
@@ -0,0 +1,59 @@
+import 'package:chess_game/backend%20files/chess_data.dart';
+import 'package:chess_game/backend%20files/model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Chess extends StatefulWidget {
+  const Chess({Key? key}) : super(key: key);
+
+  @override
+  _ChessState createState() => _ChessState();
+}
+
+class _ChessState extends State<Chess> {
+  @override
+  Widget build(BuildContext context) {
+    ChessData chessData = Provider.of<ChessData>(context);
+    Size size = MediaQuery.of(context).size;
+    double widthOfPlace = size.width / 8;
+    List<Widget> places = chessData.chess
+        .map(
+          (place) => GestureDetector(
+            onTap: () {
+              chessData.onTapPlace(place);
+            },
+            child: Container(
+              width: widthOfPlace,
+              height: widthOfPlace,
+              decoration: BoxDecoration(
+                border:
+                    Border.all(color: chessData.borderColor(place), width: 3.0),
+                color: place.stateColor == StateColor.susKill
+                    ? Colors.red[200]
+                    : chessData.chessBoard[place.position],
+              ),
+              child: chessData.placeContent(place),
+            ),
+          ),
+        )
+        .toList();
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Container(
+          alignment: Alignment.center,
+          height: size.width + 20,
+          width: size.width,
+          child: GridView.count(
+            padding: EdgeInsets.only(bottom: 10),
+            crossAxisCount: 8,
+            // scrollDirection: Axis.horizontal,
+            children: places,
+            physics: NeverScrollableScrollPhysics(),
+          ),
+        ),
+      ),
+    );
+  }
+}
